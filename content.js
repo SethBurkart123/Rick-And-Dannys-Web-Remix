ca1ceb7eeae7b44f4ef8cbb9f7eb81d06d86887c
Seth Burkart <SethBurkart7@gmail.com>
2023-02-22 09:40:50 +1100

    add video support

--- content.js
+++ content.js
@@ -1,24 +1,39 @@
-const nicolasCage =
+const dannyDevito =
   [
     "https://raw.githubusercontent.com/SethBurkart123/Danidevito/main/devito.jpg",
-    "https://raw.githubusercontent.com/SethBurkart123/Danidevito/main/devito2.jpg"
+    "https://raw.githubusercontent.com/SethBurkart123/Danidevito/main/devito2.jpg",
+    "https://raw.githubusercontent.com/SethBurkart123/Danidevito/main/devito3.jpg",
+    "https://raw.githubusercontent.com/SethBurkart123/Danidevito/main/devito4.avif"
   ]
+rickRoll = 
 
 const changeImg = () => {
   const imgs = document.querySelectorAll("img");
   const images = document.querySelectorAll("image");
+  const videos = document.querySelectorAll(".video");
+
+  videos.forEach((video) => {
+    if(video.rickroll.includes('rickrolled')) {
+      if(video.src.includes("blob")) {
+        video.src = rickRollYoutube;
+        video.setAttribute('rickrolled', 'true');
+      }
+    }
+  });
 
   imgs.forEach((img) => {
-    if (!nicolasCage.includes(img.src)) {
-      img.src = nicolasCage[Math.floor(Math.random()*nicolasCage.length)];
+    if (!dannyDevito.includes(img.src)) {
+      img.src = dannyDevito[Math.floor(Math.random()*dannyDevito.length)];
       img.srcset = "";
-      img.style = 'object-fit: contain !important;';
+      img.style = 'object-fit: cover !important;';
     }
   });
 
   images.forEach((img) => {
-    img.src = nicolasCage[Math.floor(Math.random()*nicolasCage.length)];
-    img.style = 'object-fit: contain !important;';
+    if (!dannyDevito.includes(img.src)) {
+      img.src = dannyDevito[Math.floor(Math.random()*dannyDevito.length)];
+      img.style = 'object-fit: cover !important;';
+    }
   });
 };
 
